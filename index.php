ccf8fe4a2c163cf1cfaeb782200811acd86d6eb9
ErikonMACC <erik.roblox0602@gmail.com>
2024-07-29 21:57:18 -0300

    Created the HTML structure

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Projeto SUNCAR</title>
+</head>
+
+<body>
+    <header>
+        <a href="index.php">
+            <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
+        </a>
+        <nav>
+            <ul>
+                <li class="buttonNormal"><a href="#">Sobre o Projeto</a></li>
+                <li class="buttonNormal"><a href="#">Protótipo</a></li>
+                <li class="buttonNormal"><a href="#">Colaboradores</a></li>
+                <li class="buttonVote"><a href="#">Vote agora!</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="hero">
+        <div class="box">
+            <div class="boxText">
+                <p class="titleOne">Projeto de Sustentabilidade</p>
+                <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p class="buttonVote"><a href="#">Veja o projeto agora!</a></p>
+            </div>
+            <img src="./assets/images/imageHero.png" alt="image-hero">
+        </div>
+    </div>
+
+    <div class="article">
+        <div class="box">
+            <div class="imageArticle">
+                <img src="./assets/images/imageArticle1.png" alt="solarPanel">
+            </div>
+            <div class="boxText">
+                <p class="titleTwo">Sobre o nosso projeto <span class="yellow">SUN</span><span class="lightGreen">CAR</span></p>
+                <p class="paragraf">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="boxText">
+                <p class="titleTwo">Para qual <span class="green">afinidade</span> ele serve?</p>
+                <p class="paragraf">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div class="imageArticle">
+                <img src="./assets/images/imageArticle2.png" alt="questionIcon">
+            </div>
+        </div>
+    </div>
+
+    <div class="prototype">
+        <img src="./assets/images/prototype" alt="image-hero">
+        <div class="boxText">
+            <p class="titleOne">Um prótotipo do <span class="yellow">SUN</span><span class="lightGreen">CAR</span></p>
+            <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="buttonVote"><a href="#">Veja o projeto agora!</a></p>
+        </div>
+    </div>
+
+    <div class="vote">
+        <div class="box">
+            <div class="imageArticle">
+                <img src="./assets/images/imageArticle1.png" alt="solarPanel">
+            </div>
+            <div class="boxText">
+                <p class="titleTwo">Gostou do nosso projeto? De seu <span class="green">feedback</span>!!!</p>
+                <p class="paragraf">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+        <div class="voteForms"></div>
+    </div>
+
+    <div class="collaborators">
+        <p class="titleOne">Colaboradores do projeto</p>
+        <div class="images">
+            <div class="collaborator">
+                <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                <p class="name">Colaborador 1</p>
+                <p class="insta">@erikon.macc</p>
+            </div>
+            <div class="collaborator">
+                <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                <p class="name">Colaborador 1</p>
+                <p class="insta">@erikon.macc</p>
+            </div>
+            <div class="collaborator">
+                <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                <p class="name">Colaborador 1</p>
+                <p class="insta">@erikon.macc</p>
+            </div>
+            <div class="collaborator">
+                <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                <p class="name">Colaborador 1</p>
+                <p class="insta">@erikon.macc</p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <a href="index.php">
+            <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
+        </a>
+        <p class="copyright">© Copyright SUNCAR by students of ETEC Sales Gomes</p>
+        <img src="./assets/images/etecSalesGomesIcon.png" alt="Logo-ETEC">
+    </footer>
+
+
+
+    <script src="./assets/main.js"></script>
+</body>
+
+</html>

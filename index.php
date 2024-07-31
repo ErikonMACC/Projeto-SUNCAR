1c4891c6b8f543bc8edf6213972f91dd5194cad8
ErikonMACC <erik.roblox0602@gmail.com>
2024-07-30 21:15:29 -0300

    Started CSS

--- index.php
+++ index.php
@@ -5,33 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Projeto SUNCAR</title>
 </head>
 
 <body>
-    <header>
-        <a href="index.php">
+    <header class="row">
+        <a class="col-2 offset-1" href="index.php">
             <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
         </a>
-        <nav>
-            <ul>
-                <li class="buttonNormal"><a href="#">Sobre o Projeto</a></li>
-                <li class="buttonNormal"><a href="#">Protótipo</a></li>
-                <li class="buttonNormal"><a href="#">Colaboradores</a></li>
-                <li class="buttonVote"><a href="#">Vote agora!</a></li>
+        <nav class="col-8">
+            <ul class="row">
+                <li class="col-2"><a href="#" class="buttonNormal">Sobre o Projeto</a></li>
+                <li class="col-2 offset-1"><a href="#" class="buttonNormal">Protótipo</a></li>
+                <li class="col-2 offset-1"><a href="#" class="buttonNormal">Colaboradores</a></li>
+                <li class="offset-2 col-2"><a href="#" class="buttonVote">Vote agora!</a></li>
             </ul>
         </nav>
     </header>
 
-    <div class="hero">
-        <div class="box">
-            <div class="boxText">
-                <p class="titleOne">Projeto de Sustentabilidade</p>
-                <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p class="buttonVote"><a href="#">Veja o projeto agora!</a></p>
+    <div class="hero row">
+        <div class="box col-10 offset-1">
+            <div class="row">
+                <div class="boxText col-8">
+                    <p class="titleOne">Projeto de Sustentabilidade</p>
+                    <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p><a href="#" class="buttonVote">Veja o projeto agora!</a></p>
+                </div>
+                <img class="offset-1 col-3 imageHero" src="./assets/images/imageHero.png" alt="image-hero">
             </div>
-            <img src="./assets/images/imageHero.png" alt="image-hero">
         </div>
     </div>
 
@@ -62,7 +65,7 @@
     </div>
 
     <div class="prototype">
-        <img src="./assets/images/prototype" alt="image-hero">
+        <img src="./assets/images/prototype.png" alt="image-prototype">
         <div class="boxText">
             <p class="titleOne">Um prótotipo do <span class="yellow">SUN</span><span class="lightGreen">CAR</span></p>
             <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -73,7 +76,7 @@
     <div class="vote">
         <div class="box">
             <div class="imageArticle">
-                <img src="./assets/images/imageArticle1.png" alt="solarPanel">
+                <img src="./assets/images/imageArticle3.png" alt="image-vote">
             </div>
             <div class="boxText">
                 <p class="titleTwo">Gostou do nosso projeto? De seu <span class="green">feedback</span>!!!</p>
@@ -119,8 +122,7 @@
         <img src="./assets/images/etecSalesGomesIcon.png" alt="Logo-ETEC">
     </footer>
 
-
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="./assets/main.js"></script>
 </body>
 

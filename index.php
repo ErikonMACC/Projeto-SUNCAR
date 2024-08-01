bcfb3e3a4b7841a0574933b3501191711f22fd14
ErikonMACC <erik.roblox0602@gmail.com>
2024-07-31 21:26:53 -0300

    Almost done with CSS

--- index.php
+++ index.php
@@ -11,116 +11,132 @@
 </head>
 
 <body>
-    <header class="row">
-        <a class="col-2 offset-1" href="index.php">
-            <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
-        </a>
-        <nav class="col-8">
-            <ul class="row">
-                <li class="col-2"><a href="#" class="buttonNormal">Sobre o Projeto</a></li>
-                <li class="col-2 offset-1"><a href="#" class="buttonNormal">Protótipo</a></li>
-                <li class="col-2 offset-1"><a href="#" class="buttonNormal">Colaboradores</a></li>
-                <li class="offset-2 col-2"><a href="#" class="buttonVote">Vote agora!</a></li>
-            </ul>
-        </nav>
+    <header>
+        <div class="container">
+            <div class="header row">
+                <a class="col-3" href="index.php">
+                    <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
+                </a>
+                <nav class="col-9">
+                    <ul>
+                        <li><a href="#" class="buttonNormal">Sobre o Projeto</a></li>
+                        <li><a href="#" class="buttonNormal">Protótipo</a></li>
+                        <li><a href="#" class="buttonNormal">Colaboradores</a></li>
+                        <li><a href="#" class="buttonVote">Vote agora!</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </header>
 
-    <div class="hero row">
-        <div class="box col-10 offset-1">
-            <div class="row">
+    <div class="hero">
+        <div class="container">
+            <div class="box row">
                 <div class="boxText col-8">
                     <p class="titleOne">Projeto de Sustentabilidade</p>
-                    <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p><a href="#" class="buttonVote">Veja o projeto agora!</a></p>
+                    <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p class="buttonHero"><a href="#" class="buttonVote">Veja o projeto agora!</a></p>
                 </div>
                 <img class="offset-1 col-3 imageHero" src="./assets/images/imageHero.png" alt="image-hero">
             </div>
         </div>
     </div>
 
-    <div class="article">
-        <div class="box">
-            <div class="imageArticle">
+    <div class="article container">
+        <div class="box row">
+            <div class="imageArticle col-3">
                 <img src="./assets/images/imageArticle1.png" alt="solarPanel">
             </div>
-            <div class="boxText">
+            <div class="boxText offset-1 col-8">
                 <p class="titleTwo">Sobre o nosso projeto <span class="yellow">SUN</span><span class="lightGreen">CAR</span></p>
-                <p class="paragraf">
+                <p class="paragrafTwo">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
         </div>
 
-        <div class="box">
-            <div class="boxText">
+
+        <div class="box row">
+            <div class="boxText col-8">
                 <p class="titleTwo">Para qual <span class="green">afinidade</span> ele serve?</p>
-                <p class="paragraf">
+                <p class="paragrafTwo">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
-            <div class="imageArticle">
+            <div class="imageArticle offset-1 col-3">
                 <img src="./assets/images/imageArticle2.png" alt="questionIcon">
             </div>
         </div>
     </div>
 
-    <div class="prototype">
-        <img src="./assets/images/prototype.png" alt="image-prototype">
-        <div class="boxText">
-            <p class="titleOne">Um prótotipo do <span class="yellow">SUN</span><span class="lightGreen">CAR</span></p>
-            <p class="paragraf">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p class="buttonVote"><a href="#">Veja o projeto agora!</a></p>
+    <div class="prototype row">
+        <div class="box col-10 offset-1">
+            <img class="col-4" src="./assets/images/prototype.png" alt="image-prototype">
+            <div class="boxText col-7 offset-1">
+                <p class="titleOne">Um prótotipo do <span class="yellow">SUN</span><span class="lightGreen">CAR</span></p>
+                <p class="paragrafTwo">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
         </div>
     </div>
 
-    <div class="vote">
-        <div class="box">
-            <div class="imageArticle">
-                <img src="./assets/images/imageArticle3.png" alt="image-vote">
-            </div>
-            <div class="boxText">
+    <div class="vote container">
+        <div class="box row">
+            <div class="boxText col-8">
                 <p class="titleTwo">Gostou do nosso projeto? De seu <span class="green">feedback</span>!!!</p>
-                <p class="paragraf">
+                <p class="paragrafTwo">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+            </div>
+            <div class="imageArticle col-3 offset-1">
+                <img src="./assets/images/imageArticle3.png" alt="image-vote">
             </div>
         </div>
         <div class="voteForms"></div>
     </div>
 
     <div class="collaborators">
-        <p class="titleOne">Colaboradores do projeto</p>
-        <div class="images">
-            <div class="collaborator">
-                <img src="./assets/images/erikombi.png" alt="collaborator-one">
-                <p class="name">Colaborador 1</p>
-                <p class="insta">@erikon.macc</p>
+        <div class="container">
+            <div class="row rowTitle">
+                <p class="titleTwo col-12">Colaboradores do projeto</p>
             </div>
-            <div class="collaborator">
-                <img src="./assets/images/erikombi.png" alt="collaborator-one">
-                <p class="name">Colaborador 1</p>
-                <p class="insta">@erikon.macc</p>
-            </div>
-            <div class="collaborator">
-                <img src="./assets/images/erikombi.png" alt="collaborator-one">
-                <p class="name">Colaborador 1</p>
-                <p class="insta">@erikon.macc</p>
-            </div>
-            <div class="collaborator">
-                <img src="./assets/images/erikombi.png" alt="collaborator-one">
-                <p class="name">Colaborador 1</p>
-                <p class="insta">@erikon.macc</p>
+            <div class="row">
+                <div class="images">
+                    <div class="collaborator">
+                        <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                        <p class="name">Colaborador 1</p>
+                        <p class="insta">@erikon.macc</p>
+                    </div>
+                    <div class="collaborator">
+                        <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                        <p class="name">Colaborador 1</p>
+                        <p class="insta">@erikon.macc</p>
+                    </div>
+                    <div class="collaborator">
+                        <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                        <p class="name">Colaborador 1</p>
+                        <p class="insta">@erikon.macc</p>
+                    </div>
+                    <div class="collaborator">
+                        <img src="./assets/images/erikombi.png" alt="collaborator-one">
+                        <p class="name">Colaborador 1</p>
+                        <p class="insta">@erikon.macc</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <footer>
-        <a href="index.php">
-            <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
-        </a>
-        <p class="copyright">© Copyright SUNCAR by students of ETEC Sales Gomes</p>
-        <img src="./assets/images/etecSalesGomesIcon.png" alt="Logo-ETEC">
-    </footer>
+    <div class="footer">
+        <div class="container">
+            <footer>
+                <a href="index.php">
+                    <img src="./assets/images/logo.png" alt="Logo-SUNCAR">
+                </a>
+                <p class="copyright">© Copyright SUNCAR by students of ETEC Sales Gomes</p>
+                <img src="./assets/images/etecSalesGomesIcon.png" alt="Logo-ETEC">
+            </footer>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="./assets/main.js"></script>
